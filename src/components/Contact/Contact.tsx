79d5f813c83d15d261a150d694cc69e2a0bd69ab
Devin Lizardi <dlizardi@berkeley.edu>
2025-11-29 13:48:09 -0500

    contact page updates

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,22 +1,25 @@
 import { Hamburger } from "../Nav/Hamburger";
-import cs from './Contact.module.scss'
+import cs from "./Contact.module.scss";
 
 export const Contact = () => {
-  return <div className={cs.frame}>
-    <div className={cs.contactPage}>
-      <Hamburger />
-      <h1 className={cs.contactTitle}>Contact</h1>
-      <p>
+  return (
+    <div className={cs.frame}>
+      <div className={cs.contactPage}>
+        <Hamburger />
+        <h1 className={cs.contactTitle}>Contact</h1>
+        <p>
+          <b>Isabella Guerra Uccelli </b>is a spanish and english language
+          writer and researcher
+        </p>
+        <img rel="preload" src="/isabella.jpg" />
         <br />
-        Isabella Guerra Uccelli
-        <br />
-        <br />
-
-        Email:
-        <br />
-
-        iguerrauccelli@gmail.com
-      </p>
-    </div></div>
-
+        <p>
+          Reach her via{" "}
+          <a className={cs.email} href="mailto:iguerrauccelli@gmail.com">
+            email
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
