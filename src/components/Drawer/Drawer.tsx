fa5784acc3009ea92b137477bcf832ef56c4e81f
Devin Lizardi <dlizardi@berkeley.edu>
2025-11-30 13:33:07 -0500

    redo height, simplify menu, hamburgesa

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -2,48 +2,30 @@ import { NavLink } from "react-router";
 import cs from "./Drawer.module.scss";
 import { createPortal } from "react-dom";
 import { works } from "../../works/works";
+import { formatRoute } from "../WorkList/formatRoute";
 
 export const Drawer = ({ close }: { close: () => void }) => {
   return createPortal(
     <nav className={cs.menu}>
-      <div className={cs.header}>
-        <button className={cs.closeX} onClick={close}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="15"
-            viewBox="0 0 16 15"
-            fill="none"
-          >
-            <line
-              x1="1.12129"
-              y1="0.707078"
-              x2="14.5563"
-              y2="14.1421"
-              stroke="var(--cream)"
-              strokeWidth="2"
-            />
-            <line
-              x1="0.707078"
-              y1="14.1423"
-              x2="14.1421"
-              y2="0.70723"
-              stroke="var(--cream)"
-              strokeWidth="2"
-            />
-          </svg>
-        </button>
-        <NavLink to="/">
-          Isa
-          <span className={cs.block} />
-        </NavLink>
-      </div>
+      <div className={cs.header}></div>
       <section className={cs.menuListWrapper}>
         <ul className={cs.menuList}>
-          <li>Contact</li>
-          {works.map((work) => (
-            <li>{work.title}</li>
-          ))}
+          <li onClick={close}>
+            <NavLink to={"/"}>Home</NavLink>
+          </li>
+          <li onClick={close}>
+            <NavLink to={"/contact"}>Contact</NavLink>
+          </li>
+          {works.map((work) => {
+            const { title } = work;
+            return (
+              <li onClick={close}>
+                <NavLink to={`/${formatRoute(title)}`} key={title}>
+                  {title}
+                </NavLink>
+              </li>
+            );
+          })}
         </ul>
       </section>
     </nav>,
--- src/components/Nav/Hamburger.tsx
+++ src/components/Nav/Hamburger.tsx
@@ -1,41 +1,19 @@
 import cs from "./Nav.module.scss";
 import { Drawer } from "../Drawer/Drawer";
 import { useState } from "react";
+import cn from "classnames";
 
 export const Hamburger = () => {
   const [menu, setMenu] = useState(false);
   return (
     <>
-      <button onClick={() => setMenu(true)}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="19"
-          height="14"
-          viewBox="0 0 19 14"
-          fill="none"
-        >
-          <line
-            className={cs.hamburgerLine}
-            y1="1"
-            x2="19"
-            y2="1"
-            strokeWidth="2"
-          />
-          <line
-            y1="13"
-            x2="19"
-            y2="13"
-            strokeWidth="2"
-            className={cs.hamburgerLine}
-          />
-          <line
-            y1="7"
-            x2="19"
-            y2="7"
-            strokeWidth="2"
-            className={cs.hamburgerLine}
-          />
-        </svg>
+      <button
+        className={cn(cs.hamburger, { [cs.open]: menu })}
+        onClick={() => setMenu(!menu)}
+      >
+        <span className={cn(cs.hamburgerLine, { [cs.open]: menu })} />
+        <span className={cn(cs.hamburgerLine, { [cs.open]: menu })} />
+        <span className={cn(cs.hamburgerLine, { [cs.open]: menu })} />
       </button>
       {menu && <Drawer close={() => setMenu(false)} />}
     </>
