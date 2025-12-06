1aa2ed05b35de689b3cca9aac50feb9b6a3e970a
Devin Lizardi <dlizardi@berkeley.edu>
2025-12-06 15:26:36 -0500

    reader desktop fixes

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -7,11 +7,13 @@ export const Contact = () => {
       <div className={cs.contactPage}>
         <Hamburger />
         <p>
-          <b>Isabella Guerra Uccelli </b>is a Spanish and English language
-          writer and researcher. Her creative writing vary in style and are
-          inspired from personal experience. Some of her writing is bilingual,
+          <b>Isabella Guerra Uccelli </b>is a bilingual, Spanish and English
+          language writer and researcher. Her creative writing varies in style
+          and her works are inspired from personal experience. In some of her
+          writing, she chooses to intercalate both of her native languages,
           transitioning from one language to another. Born in Boston,
-          Massachusetts, she is of Peruvian and Puerto Rican background.
+          Massachusetts, she is the daughter of Peruvian and Puerto Rican
+          parents.
         </p>
         <img rel="preload" src="/isabella.jpg" />
         <br />
