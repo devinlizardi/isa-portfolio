9d8269c471fcfe784c5e4f5d9386fee5b3a8ebf0
Devin Lizardi <dlizardi@berkeley.edu>
2025-12-14 13:57:47 -0500

    espanol meta desc

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 import { Outlet } from "react-router";
+import { getBrowserLanguage } from "./helpers/getBrowserLanguage";
+import { useEffect } from "react";
 
 function App() {
+  const language = getBrowserLanguage();
+  const espanol = language.includes("es");
+
+  useEffect(() => {
+    const ogDescription = document.querySelector(
+      'meta[property="og:description"]',
+    );
+    if (espanol) {
+      ogDescription?.setAttribute(
+        "content",
+        "Isabella Guerra Uccelli es una escritora e investigadora bilingüe en inglés y español.",
+      );
+    }
+  }, [espanol]);
+
   return (
     <>
       <Outlet />
